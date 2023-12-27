a1470c7fb35ae3fbaa5271aa5640a10ab050b826
LuccasAguiarLCC <luccasaguiarlcc@gmail.com>
2023-12-27 20:41:17 -0300

    test: função para converter um arquivo excel

--- run.py
+++ run.py
@@ -5,3 +5,22 @@ import pandas as pd
 from io import BytesIO
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
+
+def excel_pdf(arquivo_excel, output_pdf):
+
+    arquivo_excel = pd.read_excel("nome do arquivo")
+
+    # Define as colunas e depois cria um gráfico, para ser salvo posteriormente (get_figure)
+    x_column, y_column = arquivo_excel.columns[:10]
+    plot = arquivo_excel.plot(x=x_column, y=y_column, kind='bar').get_figure()
+
+    # Buffer para lermos o gráfico criado e transforma-lo em um png
+    buffer = BytesIO()
+    plot.savefig(buffer, format='png')
+    buffer.seek(0)
+
+    # Processo para salvar o PDF através da imagem 'desenhada' pelo canvas
+    c = canvas.Canvas(output_pdf, pagesize=letter)
+    c.drawImage(buffer, 100, 400, width=400, height=300)
+    c.showPage()
+    c.save()
