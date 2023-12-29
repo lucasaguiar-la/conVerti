0fea38b5310111c3fae792c4df3b7cd5562d7ee5
LuccasAguiarLCC <luccasaguiarlcc@gmail.com>
2023-12-29 12:32:50 -0300

    test: testando funcionalidades

--- run.py
+++ run.py
@@ -1,26 +1,33 @@
 import os
-from docx import Document
-from PIL import Image
 import pandas as pd
+from PIL import Image
 from io import BytesIO
+from docx import Document
+import matplotlib.pyplot as plt
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
+from reportlab.lib.utils import ImageReader
 
 def excel_pdf(arquivo_excel, output_pdf):
 
-    arquivo_excel = pd.read_excel("nome do arquivo")
+    arquivo_excel = pd.read_excel("nome do arquivo.xlsx")
 
-    # Define as colunas e depois cria um gráfico, para ser salvo posteriormente (get_figure)
-    x_column, y_column = arquivo_excel.columns[:10]
-    plot = arquivo_excel.plot(x=x_column, y=y_column, kind='bar').get_figure()
-
-    # Buffer para lermos o gráfico criado e transforma-lo em um png
-    buffer = BytesIO()
-    plot.savefig(buffer, format='png')
-    buffer.seek(0)
-
-    # Processo para salvar o PDF através da imagem 'desenhada' pelo canvas
     c = canvas.Canvas(output_pdf, pagesize=letter)
-    c.drawImage(buffer, 100, 400, width=400, height=300)
+
+    x, y = 100, 800
+
+    for _, row in arquivo_excel.iterrows():
+        for col_name, value in row.items():
+            c.drawString(x, y, f"{col_name}:")
+            x += 80
+
+            c.drawString(x, y, str(value))
+            x += 120
+
+        y -= 20
+        x = 100
+
     c.showPage()
     c.save()
+
+excel_pdf("nome do arquivo.xlsx", "teste.pdf")
