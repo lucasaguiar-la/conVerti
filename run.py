537859f49b47aa031c6598140ecc1a50fa52f36a
LuccasAguiarLCC <luccasaguiarlcc@gmail.com>
2023-12-29 13:23:20 -0300

    feat: conversão de arquivo .xlsx p/ .pdf

--- run.py
+++ run.py
@@ -1,33 +1,12 @@
 import os
-import pandas as pd
-from PIL import Image
-from io import BytesIO
-from docx import Document
-import matplotlib.pyplot as plt
-from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import letter
-from reportlab.lib.utils import ImageReader
+from win32com import client
 
-def excel_pdf(arquivo_excel, output_pdf):
+cod_dir = os.path.dirname(os.path.abspath(__file__))
+arquivo_dir = os.path.join(cod_dir, "arquivo")
+pdf_dir = os.path.join(cod_dir, "final.pdf")
+excel = client.Dispatch("Excel.Application")
 
-    arquivo_excel = pd.read_excel("nome do arquivo.xlsx")
+sheets = excel.Workbooks.Open(arquivo_dir)
+work_sheets = sheets.Worksheets[0]
 
-    c = canvas.Canvas(output_pdf, pagesize=letter)
-
-    x, y = 100, 800
-
-    for _, row in arquivo_excel.iterrows():
-        for col_name, value in row.items():
-            c.drawString(x, y, f"{col_name}:")
-            x += 80
-
-            c.drawString(x, y, str(value))
-            x += 120
-
-        y -= 20
-        x = 100
-
-    c.showPage()
-    c.save()
-
-excel_pdf("nome do arquivo.xlsx", "teste.pdf")
+work_sheets.ExportAsFixedFormat(0, pdf_dir)
