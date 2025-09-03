8974b9c4506584802803fe54234cf7264e498e1d
lucasaguiar-la <lucas.aguiar.ads@gmail.com>
2025-09-03 15:32:03 -0300

    [TEST]: Testando funcionalidade de intervalo para PDFs

--- src/converte_excel.py
+++ src/converte_excel.py
@@ -2,9 +2,9 @@ from src.utils import definir_caminho
 from win32com import client
 import os
 
-def converter_excel():
+def converter_excel(aplicar_intervalo=False):
     index = 0
-    loop = 0
+    loop = 1
     diretorio_arquivo = definir_caminho("../arquivos/originais/excel/arquivo.xlsx")
     diretorio_destino = definir_caminho("../arquivos/convertidos/excel/")
 
@@ -14,22 +14,25 @@ def converter_excel():
     arquivo_saida = excel.Workbooks.Add()
 
     print("Iniciando conversão...")
-    while True:
-        try:
-            planilha_ativa = planilhas.Worksheets[index]
-            planilha_ativa.Copy(None, arquivo_saida.Sheets(arquivo_saida.Sheets.Count))
-            print(f"\nConvertendo planilha/sheet {index+1}")
-            index += 1
-        except Exception as e:
-            print(f"\nAviso: {e}\n")
-            print("Conversão de Excel para PDF concluída!")
-            loop + 1
-            break
-    pdf_dir = os.path.join(diretorio_destino, f"Arquivo_excel_{loop}.pdf")
-    arquivo_saida.ExportAsFixedFormat(0, pdf_dir, 0, 1) 
+    if not aplicar_intervalo:
+        while True:
+            try:
+                planilha_ativa = planilhas.Worksheets[index]
+                planilha_ativa.Copy(None, arquivo_saida.Sheets(arquivo_saida.Sheets.Count))
+                print(f"\nConvertendo planilha/sheet {index+1}")
+                index += 1
+            except Exception as e:
+                print(f"\nAviso: {e}\n")
+                print("Conversão de Excel para PDF concluída!")
+                loop + 1
+                break
+        pdf_dir = os.path.join(diretorio_destino, f"ArquivoExcel_{loop}.pdf")
+        arquivo_saida.ExportAsFixedFormat(0, pdf_dir, 0, 1) 
 
-    arquivo_saida.Saved = True
-    planilhas.Close(False)
-    excel.Quit()
+        arquivo_saida.Saved = True
+        planilhas.Close(False)
+        excel.Quit()
+    else:
+        print("Converter um PDF por sheet!")
 
     return True
