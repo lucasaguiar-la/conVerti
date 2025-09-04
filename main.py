b41272431afed32132552a90ce7d8acdb7ad2d3b
lucasaguiar-la <lucas.aguiar.ads@gmail.com>
2025-09-04 09:54:06 -0300

    [FEAT]: Parâmetro para N arquivos PDF (intervalo)

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from src.converte_excel import converter_excel
 
 if __name__ == "__main__":
     print("Iniciando conversão de arquivo excel...")
-    processo = converter_excel()
+    processo = converter_excel(aplicar_intervalo=False)
 
     if processo:
         print("Processo de conversão concluído!")
+    else:
+        print("Algo deu errado!")
--- src/converte_excel.py
+++ src/converte_excel.py
@@ -3,36 +3,29 @@ from win32com import client
 import os
 
 def converter_excel(aplicar_intervalo=False):
-    index = 0
-    loop = 1
     diretorio_arquivo = definir_caminho("../arquivos/originais/excel/arquivo.xlsx")
     diretorio_destino = definir_caminho("../arquivos/convertidos/excel/")
 
     print("Instanciando aplicação Excel...")
     excel = client.Dispatch("Excel.Application")
     planilhas = excel.Workbooks.Open(diretorio_arquivo)
-    arquivo_saida = excel.Workbooks.Add()
 
     print("Iniciando conversão...")
     if not aplicar_intervalo:
-        while True:
-            try:
-                planilha_ativa = planilhas.Worksheets[index]
-                planilha_ativa.Copy(None, arquivo_saida.Sheets(arquivo_saida.Sheets.Count))
-                print(f"\nConvertendo planilha/sheet {index+1}")
-                index += 1
-            except Exception as e:
-                print(f"\nAviso: {e}\n")
-                print("Conversão de Excel para PDF concluída!")
-                loop + 1
-                break
-        pdf_dir = os.path.join(diretorio_destino, f"ArquivoExcel_{loop}.pdf")
-        arquivo_saida.ExportAsFixedFormat(0, pdf_dir, 0, 1) 
+        arquivo_saida = excel.Workbooks.Add()
+        for index in range(planilhas.Worksheets.Count):
+            planilhas.Worksheets[index].Copy(None, arquivo_saida.Sheets(arquivo_saida.Sheets.Count))
 
-        arquivo_saida.Saved = True
-        planilhas.Close(False)
-        excel.Quit()
+        diretorio_pdf = os.path.join(diretorio_destino, f"ArquivoExcel.pdf")
+        arquivo_saida.ExportAsFixedFormat(0, diretorio_pdf, 0, 1) 
+        arquivo_saida.Close(False)
+
     else:
-        print("Converter um PDF por sheet!")
+        for index, planilha in enumerate(planilhas.Worksheets, start=1):
+            diretorio_pdf = os.path.join(diretorio_destino, f"ArquivoExcel_intervalo_{index}")
+            planilha.ExportAsFixedFormat(0, diretorio_pdf, 0, 1)
+
+    planilhas.Close(False)
+    excel.Quit()
 
     return True
